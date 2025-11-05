8d9ee4e13af6ba95277036eed125174544cf8fe7
TheoChabb <234576250+TheoChabb@users.noreply.github.com>
2025-11-05 21:17:09 +0100

    Update script2.js

--- scripts/script2.js
+++ scripts/script2.js
@@ -1,18 +1,22 @@
+window.onload = function() {
+    afficherHeure();
+};
+
 function toggleMenu() {
-  const menu = document.getElementById('menu');
-  menu.classList.toggle('open');
+    const menu = document.getElementById('menu');
+    menu.classList.toggle('open');
 }
 
 function afficherHeure() {
-  const now = new Date();
-  let heure = now.getHours().toString().padStart(2, '0');
-  let minute = now.getMinutes().toString().padStart(2, '0');
-  let second = now.getSeconds().toString().padStart(2, '0');
-
-  const heureActuelle = `${heure}:${minute}:${second}`;
-  document.getElementById("date").textContent = heureActuelle;
-  document.getElementById("dateTel").textContent = heureActuelle;
-
-  alert(heureActuelle);
-  setTimeout(afficherHeure, 1000);
+    const now = new Date();
+    let heure = now.getHours().toString().padStart(2, '0');
+    let minute = now.getMinutes().toString().padStart(2, '0');
+    let second = now.getSeconds().toString().padStart(2, '0');
+    
+    const heureActuelle = `${heure}:${minute}:${second}`;
+    document.getElementById("date").textContent = heureActuelle;
+    document.getElementById("dateTel").textContent = heureActuelle;
+    
+    alert(heureActuelle);
+    setTimeout(afficherHeure, 1000);
 }
