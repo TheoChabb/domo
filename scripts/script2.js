10472c3f4ea9229af9460b8e64090b1ce0ec62e3
TheoChabb <234576250+TheoChabb@users.noreply.github.com>
2025-11-05 21:02:45 +0100

    Update script2.js

--- scripts/script2.js
+++ scripts/script2.js
@@ -1,1 +1,20 @@
+function toggleMenu() {
+  const menu = document.getElementById('menu');
+  const page = document.getElementById('page');
+  menu.classList.toggle('open');
+  page.classList.toggle('open');
+  page.classList.toggle('open2');
+}
 
+function afficherHeure() {
+  const now = new Date();
+  let heure = now.getHours().toString().padStart(2, '0');
+  let minute = now.getMinutes().toString().padStart(2, '0');
+  let second = now.getSeconds().toString().padStart(2, '0');
+
+  const heureActuelle = `${heure}:${minute}:${second}`;
+  document.getElementById("date").textContent = heureActuelle;
+  document.getElementById("dateTel").textContent = heureActuelle;
+
+  setTimeout(afficherHeure, 1000);
+}
