3a2877804f036729e202e19a5a80228124863e31
TheoChabb <234576250+TheoChabb@users.noreply.github.com>
2025-11-23 13:10:05 +0100

    Update script4.js

--- scripts/script4.js
+++ scripts/script4.js
@@ -1,1 +1,23 @@
+setInterval(loadValue, 1000);
 
+function loadValue() {
+    let xhrTemp = new XMLHttpRequest();
+    xhrTemp.open("GET", "http://domo.theo.free.fr/data_github/temperature_chambre.txt");
+    xhrTemp.onreadystatechange = function() {
+        if (xhrTemp.readyState === 4 && xhrTemp.status === 200) {
+            const tempInt = xhrTemp.responseText;
+            document.getElementById('tempInt').textContent = tempInt || "Chargement...";
+        }
+    };
+    xhrTemp.send();
+
+    let xhrHumi = new XMLHttpRequest();
+    xhrHumi.open("GET", "http://domo.theo.free.fr/data_github/humidite_chambre.txt");
+    xhrHumi.onreadystatechange = function() {
+        if (xhrHumi.readyState === 4 && xhrHumi.status === 200) {
+            const humiInt = xhrHumi.responseText;
+            document.getElementById('humiInt').textContent = humiInt || "Chargement...";
+        }
+    };
+    xhrHumi.send();
+}
