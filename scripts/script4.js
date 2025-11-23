0246a7b496eb76fb827e7304b6067a3182ad1de9
TheoChabb <234576250+TheoChabb@users.noreply.github.com>
2025-11-23 13:11:43 +0100

    Update script4.js

--- scripts/script4.js
+++ scripts/script4.js
@@ -2,7 +2,7 @@ setInterval(loadValue, 1000);
 
 function loadValue() {
     let xhrTemp = new XMLHttpRequest();
-    xhrTemp.open("GET", "http://domo.theo.free.fr/data_github/temperature_chambre.txt");
+    xhrTemp.open("GET", "https://corsproxy.io/?http://domo.theo.free.fr/data_github/temperature_chambre.txt");
     xhrTemp.onreadystatechange = function() {
         if (xhrTemp.readyState === 4 && xhrTemp.status === 200) {
             const tempInt = xhrTemp.responseText;
@@ -12,7 +12,7 @@ function loadValue() {
     xhrTemp.send();
 
     let xhrHumi = new XMLHttpRequest();
-    xhrHumi.open("GET", "http://domo.theo.free.fr/data_github/humidite_chambre.txt");
+    xhrHumi.open("GET", "https://corsproxy.io/?http://domo.theo.free.fr/data_github/humidite_chambre.txt");
     xhrHumi.onreadystatechange = function() {
         if (xhrHumi.readyState === 4 && xhrHumi.status === 200) {
             const humiInt = xhrHumi.responseText;
